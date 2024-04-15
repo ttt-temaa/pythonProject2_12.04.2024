aecbf66cccd89058723364d66bae73cff87f606b
ttt.temaa <artem1005200776@gmail.com>
2024-04-15 10:54:03 +0300

    Adding all functions from tests.py. file to test_funcs.py file using pytest refactor

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -8,14 +8,14 @@ from src.widget import convert_date, transform_data
 @pytest.mark.parametrize(
     "card_number,expected",
     [
-        ("1234567890123456", "1234 5678 9012 3456"),
-        ("12345678901234567", "1234 5678 9012 3456"),
-        ("12345678901234568", "1234 5678 9012 3456"),
-        ("12345678901234569", "1234 5678 9012 3456"),
-        ("12345678901234570", "1234 5678 9012 3456"),
+        ("1234567890123456", "1234 56** **** 3456"),
+        ("1234567890123457", "1234 56** **** 3457"),
+        ("1234567890123458", "1234 56** **** 3458"),
+        ("1234567890123459", "1234 56** **** 3459"),
+        ("1234567890123460", "1234 56** **** 3460"),
     ],
 )
-def test_mask_card(card_number, expected):
+def test_mask_card(card_number: str, expected: str) -> None:
     actual = mask_card(card_number)
     assert actual == expected
 
@@ -31,7 +31,7 @@ def test_mask_card(card_number, expected):
         ("2023-03-31T23:59:59.999999", "31.03.2023"),
     ],
 )
-def test_convert_date(input_date, expected):
+def test_convert_date(input_date: str, expected: str) -> None:
     actual = convert_date(input_date)
     assert actual == expected
 
@@ -39,14 +39,14 @@ def test_convert_date(input_date, expected):
 @pytest.mark.parametrize(
     "account_number,expected",
     [
-        ("1234567890", "**1234"),
-        ("12345678901", "**1234"),
-        ("123456789012", "**1234"),
-        ("1234567890123", "**1234"),
+        ("1234567890", "**7890"),
+        ("12345678901", "**8901"),
+        ("123456789012", "**9012"),
+        ("1234567890123", "**0123"),
         ("12345678901234", "**1234"),
     ],
 )
-def test_mask_account(account_number, expected):
+def test_mask_account(account_number: str, expected: str) -> None:
     actual = mask_account(account_number)
     assert actual == expected
 
@@ -54,18 +54,18 @@ def test_mask_account(account_number, expected):
 @pytest.mark.parametrize(
     "input_data,expected",
     [
-        ("Счет 1234567890", "Счет **1234"),
-        ("Карта 1234567890123456", "Карта 1234 5678 9012 3456"),
-        ("Неизвестный 1234567890123456", "Неизвестный 1234 5678 9012 3456"),
+        ("Счет 1234567890", "Счет **7890"),
+        ("Карта 1234567890123456", "Карта 1234 56** **** 3456"),
+        ("Неизвестный 1234567890123456", "Неизвестный 1234 56** **** 3456"),
     ],
 )
-def test_transform_data(input_data, expected):
+def test_transform_data(input_data: str, expected: str) -> None:
     actual = transform_data(input_data)
     assert actual == expected
 
 
 @pytest.fixture
-def input_data():
+def input_data() -> list:
     return [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
@@ -74,7 +74,7 @@ def input_data():
     ]
 
 
-def test_filter_by_state(input_data):
+def test_filter_by_state(input_data: list) -> None:
     filtered_data_executed = filter_by_state(input_data)
     assert filtered_data_executed == input_data[:2]
 
@@ -82,7 +82,7 @@ def test_filter_by_state(input_data):
     assert filtered_data_canceled == input_data[2:]
 
 
-def test_sort_dicts_by_date(input_data):
+def test_sort_dicts_by_date(input_data: list) -> None:
     sorted_descending = sort_dicts_by_date(input_data, True)
     assert sorted_descending == sorted(input_data, key=lambda x: (x["date"], "%Y-%m-%dT%H:%M:%S.%f"))[::-1]
 
